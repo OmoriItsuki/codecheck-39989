27432739d3049346ae3b7e6a658d9c21d6833087
code-project <dev@code-check.io>
2018-06-01 07:04:08 +0000

    Add new CLI related files

--- src/MainApp.cs
+++ src/MainApp.cs
@@ -7,8 +7,7 @@ public class MainApp
         for (int i = 0; i < args.Length; i++)
         {
            string output = String.Format("argv[{0}]: {1}", i, args[i]);
-           string s = Console.ReadLine();
-           Console.WriteLine("Hello " + s);
+           Console.WriteLine(output);
         }
     }
 }
